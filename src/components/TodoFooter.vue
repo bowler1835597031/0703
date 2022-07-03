77d6d013d622c45b6f92c278d74cbf3be3bc6221
bowler <1835597031@qq.com>
2022-07-03 23:00:42 +0800

    全部完成

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,21 +1,54 @@
 <template>
   <footer class="footer">
-    <span class="todo-count">剩余<strong>数量值</strong></span>
+    <span class="todo-count"
+      >剩余<strong>{{ count }}</strong></span
+    >
     <ul class="filters">
       <li>
-        <a class="selected" href="javascript:;">全部</a>
+        <a
+          :class="{ selected: isSal == 'all' }"
+          href="javascript:;"
+          @click="change('all')"
+          >全部</a
+        >
       </li>
       <li>
-        <a href="javascript:;">未完成</a>
+        <a
+          :class="{ selected: isSal == 'no' }"
+          href="javascript:;"
+          @click="change('no')"
+          >未完成</a
+        >
       </li>
       <li>
-        <a href="javascript:;">已完成</a>
+        <a
+          :class="{ selected: isSal == 'yes' }"
+          href="javascript:;"
+          @click="change('yes')"
+          >已完成</a
+        >
       </li>
     </ul>
-    <button class="clear-completed">清除已完成</button>
+    <button class="clear-completed" @click="clear">取消已完成</button>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ['count'],
+  data() {
+    return {
+      isSal: 'all',
+    };
+  },
+  methods: {
+    change(val) {
+      this.isSal = val;
+      this.$emit('filterdata', val);
+    },
+    clear() {
+      this.$emit('clear');
+    },
+  },
+};
 </script>
